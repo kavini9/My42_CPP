9d89a01e280581974c7e2f61ced4694ec8733b4f
kavini9 <kavini.w.weerasinghe@gmail.com>
2025-08-15 19:35:07 +0300

    hurry up now

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 21:01:23 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/08/14 23:15:31 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/08/15 18:17:54 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,20 @@ PhoneBook::~PhoneBook(){}
 
 void	PhoneBook::addContact()
 {
-	
+	std::string	inputs[5];
+	std::string	fieldNames[] = {
+		"first name",
+		"last name",
+		"nick name",
+		"phone number",
+		"darkest secret"
+	};
+	for (int i = 0; i < 5; ++i)
+	{
+		int attempt = 0;
+		while (attempt < 3)
+		{
+			
+		}
+	}
 }
