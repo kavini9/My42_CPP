183bbcfdc12b5cc2ad8cfab3c6d511d47509a5d7
kavini9 <kavini.w.weerasinghe@gmail.com>
2025-10-20 21:56:41 +0300

    cpp05 ex00 started

--- cpp05/ex00/ Bureaucrat.hpp
+++ cpp05/ex00/ Bureaucrat.hpp
@@ -6,7 +6,31 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 22:16:40 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/10/18 22:16:41 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/10/20 20:36:19 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+	private:
+		const std::string   _name;
+		int                 _grade;
+	
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		Bureaucrat();
+
+		std::string getName();
+		int			getGrade();
+
+		void		incrementGrade();
+		void		decrementGrade();	
+};
+
+#endif
