204f293bb3534379c344371d8dbf1e9ecc38b82d
kavini9 <kavini.w.weerasinghe@gmail.com>
2026-01-11 16:30:30 +0200

    cpp06 progressing

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,12 +6,29 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 15:20:24 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/12/31 19:25:14 by wweerasi         ###   ########.fr       */
+/*   Updated: 2026/01/08 16:14:23 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+bool	typeCheckInt(const std::string& literal) {
+	size_t pEnd;
+	int val = std::stoi(literal, &pEnd, 10);
+	if ()
+}
+
+bool	typeCheckFloat(const std::string& literal) {
+	
+}
+
+bool	typeCheckDouble(const std::string& literal) {
+	
+}
+
 void ScalarConverter::convert(const std::string& literal) {
-     
+	 bool isChar	= typeCheckChar(literal);
+	 bool isInt		= typeCheckInt(literal);
+	 bool isFloat	= typeCheckFloat(literal);
+	 bool isDouble	= typeCheckDouble(literal);
 }
