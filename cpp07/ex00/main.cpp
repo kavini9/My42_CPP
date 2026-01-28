5be354b0e6a847de44c94c40145698acd1ec5638
kavini9 <kavini.w.weerasinghe@gmail.com>
2026-01-28 23:53:14 +0200

    removed const for T in ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -10,3 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	const int e = 4;
+	const int f = 5;
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	const std::string g = "chaine3";
+	const std::string h = "chaine4";
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+	return 0;
+}
