873b3dc1529376ee4ce6d91aeca5730ec312a9fc
kavini9 <kavini.w.weerasinghe@gmail.com>
2026-02-03 22:18:32 +0200

    what I am even doing

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,14 +6,23 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 21:24:49 by wweerasi          #+#    #+#             */
-/*   Updated: 2026/02/02 21:46:33 by wweerasi         ###   ########.fr       */
+/*   Updated: 2026/02/03 21:53:16 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+
 template<typename T>
-typename T::iterator easyfind(const T& container, int )
+typename T::const_iterator easyfind(const T& container, const int val) {
+    return std::find(std::begin(container), std::end(), val);
+}
+
+template<typename T>
+typename T::iterator easyfind(T& container, const int val) {
+    return std::find(std::begin(container), std::end(container), val);
+}
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,28 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 21:24:53 by wweerasi          #+#    #+#             */
-/*   Updated: 2026/02/01 21:24:54 by wweerasi         ###   ########.fr       */
+/*   Updated: 2026/02/03 22:13:13 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include <deque>
+#include <list>
+#include <iostream>
+#include "easyfind.hpp"
+
+#define SET_B_BLK	"\033[1;30m"
+#define SET_RED		"\033[31m"
+#define RESET		"\033[0m"
+
+int main() {
+	try {
+		std::vector<int> intVec{12, 23, 34, 45, 56, 67};
+		std::deque<int> intDeq{1, 0, 2, 9, 3};
+		std::list<int> intLst{125, 45, -11, 99};
+		//write the tests
+	} 
+	catch (const std::exception& e) {
+		std::cout << SET_RED "Error: " << e.what() << RESET << std::endl;
+	}
+}
