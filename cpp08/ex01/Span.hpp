ed5159ce1dbbfa8ba539c87c2a1595b5d4f335d6
kavini9 <kavini.w.weerasinghe@gmail.com>
2026-02-13 23:19:12 +0200

    changed not much

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,7 +6,20 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 21:27:19 by wweerasi          #+#    #+#             */
-/*   Updated: 2026/02/01 21:27:20 by wweerasi         ###   ########.fr       */
+/*   Updated: 2026/02/13 22:59:25 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+class Span {
+    private:
+        
+};
+
+#endif
